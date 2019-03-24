dba40b5174cd8fe2ec8948bddd014a21b60c6aac
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-03-23 23:21:33 -0500

    Added custom-scripts.js for deliverables.

---
+++ custom-scripts.js
@@ -0,0 +1,11 @@
+/*
+============================================
+; Title:  tkarmstrong.github.io custom scripts
+; Author: Tyler K. Armstrong
+; BagetteBox Author: Copyright (c) 2018 https://github.com/feimosi/
+; Date:   23 March 2019
+; Description: Scripts for portfolio site.
+;===========================================
+*/
+
+baguetteBox.run('.compact-gallery', {animation: 'slideIn'});
