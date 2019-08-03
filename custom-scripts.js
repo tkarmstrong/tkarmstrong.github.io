bfcc79fe7cd8ebfa6ad8603e11d3b3c68b2f48ac
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-08-03 15:02:35 -0400

    Adding gallery for web-430 videos

--- custom-scripts.js
+++ custom-scripts.js
@@ -10,3 +10,17 @@
 
 // Initialize BaguetteBox
 baguetteBox.run('.compact-gallery', {animation: 'slideIn'});
+
+// Gallery Swiper Web-430
+var mySwiper = new Swiper('#slider-gallery', {
+  speed: 400,
+  navigation: {
+    nextEl: '#slider-gallery-next',
+    prevEl: '#slider-gallery-prev',
+  },
+  pagination: {
+    el: '#slider-gallery-pagination',
+    clickable: true
+  },
+  slidesPerView: 'auto'
+});
